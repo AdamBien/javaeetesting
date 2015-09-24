3e3d9cfbf18b33799a9f5783882c281ba22a7b7f
Adam Bien <abien@adam-bien.com>
2015-09-24 09:02:53 +0200

    mockito and mocks introduced

--- hello/src/test/java/com/airhacks/hello/business/order/boundary/OrderProcessorTest.java
+++ hello/src/test/java/com/airhacks/hello/business/order/boundary/OrderProcessorTest.java
@@ -1,7 +1,12 @@
 package com.airhacks.hello.business.order.boundary;
 
+import com.airhacks.hello.business.order.control.LegacyAuthenticator;
+import com.airhacks.hello.business.order.control.PaymentProcessor;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -14,16 +19,19 @@ public class OrderProcessorTest {
     @Before
     public void init() {
         this.cut = new OrderProcessor();
+        this.cut.paymentProcessor = mock(PaymentProcessor.class);
     }
 
     @Test
     public void successfulOrder() {
         this.cut.order();
+        verify(this.cut.paymentProcessor).pay();
     }
 
     @Test(expected = IllegalStateException.class)
     public void invaliduser() {
-        this.cut.authenticator = new MockAuthenticator();
+        this.cut.authenticator = mock(LegacyAuthenticator.class);
+        when(this.cut.authenticator.authenticate()).thenReturn(false);
         this.cut.order();
     }
 
