3462c7abb2b736eebe705ca30d3642bb3bb1fd3d
Adam Bien <abien@adam-bien.com>
2015-10-02 11:17:43 +0200

    json processing integrated

--- jersey-testframework/src/main/java/com/airhacks/jersey/HelloResource.java
+++ jersey-testframework/src/main/java/com/airhacks/jersey/HelloResource.java
@@ -1,6 +1,8 @@
 package com.airhacks.jersey;
 
 import javax.inject.Singleton;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -13,8 +15,10 @@ import javax.ws.rs.Path;
 public class HelloResource {
 
     @GET
-    public String ms() {
-        return " " + System.currentTimeMillis();
+    public JsonObject ms() {
+        return Json.createObjectBuilder().
+                add("time", System.currentTimeMillis()).
+                build();
     }
 
 }
--- jersey-testframework/src/test/java/com/airhacks/jersey/HelloResourceIT.java
+++ jersey-testframework/src/test/java/com/airhacks/jersey/HelloResourceIT.java
@@ -1,20 +1,20 @@
 package com.airhacks.jersey;
 
+import javax.json.JsonObject;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 /**
  *
  * @author airhacks.com
  */
-public class HelloResourceTest extends JerseyTest {
+public class HelloResourceIT extends JerseyTest {
 
     @Override
     protected Application configure() {
@@ -23,8 +23,11 @@ public class HelloResourceTest extends JerseyTest {
 
     @Test
     public void second() throws Exception {
-        Response response = target().request().get();
+        Response response = target("hello").request().get();
         assertThat(response.getStatus(), is(200));
+        JsonObject result = response.readEntity(JsonObject.class);
+        assertNotNull(result);
+        System.out.println("result = " + result);
     }
 
 }
