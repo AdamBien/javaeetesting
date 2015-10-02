eb541e933dd499b4513c8758813084af0e7616c5
Adam Bien <abien@adam-bien.com>
2015-10-02 11:27:15 +0200

    custom test rule implemented

--- monitoring-st/src/test/java/com/airhacks/mon/reporting/boundary/SnapshotsResourceIT.java
+++ monitoring-st/src/test/java/com/airhacks/mon/reporting/boundary/SnapshotsResourceIT.java
@@ -1,15 +1,12 @@
 package com.airhacks.mon.reporting.boundary;
 
 import javax.json.JsonObject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static junit.framework.Assert.assertNotNull;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -18,18 +15,12 @@ import org.junit.Test;
  */
 public class SnapshotsResourceIT {
 
-    private Client client;
-    private WebTarget tut;
-
-    @Before
-    public void initClient() {
-        this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8080/monitoring/resources/snapshots");
-    }
+    @Rule
+    public JAXRSClient provider = JAXRSClient.target("http://localhost:8080/monitoring/resources/snapshots");
 
     @Test
     public void snapshots() {
-        Response response = this.tut.request(MediaType.APPLICATION_JSON).get();
+        Response response = this.provider.tut().request(MediaType.APPLICATION_JSON).get();
         assertThat(response.getStatus(), is(200));
         JsonObject result = response.readEntity(JsonObject.class);
         assertNotNull(result);
